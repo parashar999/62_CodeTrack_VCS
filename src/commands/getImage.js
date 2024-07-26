0f7b1b735f936c19e45baf10b85b7c431c22b596
sharmatarun392000 <parashartarun392000@gmail.com>
2024-07-26 08:42:23 +0530

    15 : PUSH CODE OF `getImage` Command

--- src/commands/getImage.js
+++ src/commands/getImage.js
@@ -1,1 +1,56 @@
 // GetImage command
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { cwd } from 'process';
+import chalk from 'chalk'
+
+function handler(args) {
+    //check if the current entry is a valid pathname or not
+    //then create its entry into the index file
+    const commitId = args._[1];
+    // console.log(args);
+    const currentDir = cwd();
+    // console.log(currentDir);
+    try {
+        const history = JSON.parse(readFileSync(`${currentDir}/.codetrack/history/commitLog.json`, 'utf-8'));
+        if(!history[commitId]){
+            throw error("no such commitId present!!");
+        }
+        //check if commitId is valid or not for your current branch
+        const indexObj = JSON.parse(readFileSync(`${currentDir}/.codetrack/indices/${commitId}.json`, 'utf-8'));
+
+        for(const filePath in indexObj){
+            const hash = indexObj[filePath];
+            const data = readFileSync(`${currentDir}/.codetrack/objects/${hash}`);
+            writeFileSync(filePath,data);
+        }
+        //update index.json file
+        writeFileSync(`${currentDir}/.codetrack/index.json`,JSON.stringify(indexObj));
+
+        // make head point to this commit instead of a branch
+        const state = (JSON.parse(readFileSync(`${currentDir}/.codetrack/State.json`, 'utf-8')));
+        state["head"] = commitId;
+        state["detached"] = true;
+        writeFileSync(`${currentDir}/.codetrack/State.json`,JSON.stringify(state));
+
+
+    } catch (error) {
+        console.log(chalk.red("error in getting the commit verion: "), error);
+    }
+
+}
+function builder(yargs) {
+    return yargs
+    .option('commitId', {
+        alias: 'cId',
+        describe: 'commitId of commit to restore',
+        type: 'string'
+    });
+}
+const getImage = {
+    command: 'getImage',
+    describe: 'bring your working directory to the state it had been during a specific commit. Files that were not present during that commit or were untracked, would not be affected in any manner.',
+    builder,
+    handler
+}
+
+export default getImage;
